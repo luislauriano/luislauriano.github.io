502349d1dbe6fcda0c7e7952cf3c064536fec39e
Luis Vinicius <luislauriano@outlook.com.br>
2025-04-17 02:50:14 -0300

    Delete astro.config.js

--- astro.config.js
+++
@@ -1,7 +0,0 @@
-// astro.config.js
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://luislauriano.github.io/landing-page/',
-  base: '/landing-page/',
-});
