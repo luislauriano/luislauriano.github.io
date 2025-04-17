9096066ca3df3a8887a44a0aaab70f5083c1a4d0
Luis Vinicius <luislauriano@outlook.com.br>
2025-04-17 02:36:13 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luislauriano.github.io/landing-page/',
-  base: 'my-repo'
+  base: '/landing-page/'
   
 });
